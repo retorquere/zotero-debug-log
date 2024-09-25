5510c466003454ac2d4439caac98116de15ef5de
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-09-26 01:00:33 +0200

    file logging

--- bootstrap.ts
+++ bootstrap.ts
@@ -10,7 +10,7 @@ const {
   interfaces: Ci,
   results: Cr,
   utils: Cu,
-  Constructor: CC,
+  Constructor: Cc,
 } = Components
 
 if (typeof Zotero == 'undefined') {
@@ -50,8 +50,8 @@ async function waitForZotero() {
       var listener = {
         onOpenWindow(aWindow) {
           // Wait for the window to finish loading
-          const domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
-            .getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow)
+          const domWindow = aWindow.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+            .getInterface(Components.interfaces.nsIDOMWindowInternal || Components.interfaces.nsIDOMWindow)
           domWindow.addEventListener('load', function() {
             domWindow.removeEventListener('load', arguments.callee, false)
             if (domWindow.Zotero) {
@@ -72,8 +72,55 @@ export function install() {
   // nothing to do
 }
 
+function writer(dir: any, name: string): ((m: string) => void) {
+  const file = dir.clone()
+  file.append(name)
+  log(`logging to ${file.path}`)
+  const rawStream = Components.classes['@mozilla.org/network/file-output-stream;1'].createInstance(Components.interfaces.nsIFileOutputStream)
+  log('have raw stream')
+  rawStream.init(file, 0x02 | 0x08 | 0x20, 0o664, 0); // eslint-disable-line no-bitwise
+  log('raw stream init')
+  const outputStream = Components.classes['@mozilla.org/intl/converter-output-stream;1'].createInstance(Components.interfaces.nsIConverterOutputStream)
+  log('have output stream')
+  outputStream.init(rawStream, 'UTF-8', 1024, '?'.charCodeAt(0));
+  log('output stream init')
+
+  return (m: string) => {
+    if (!m.match(/\r?\n$/)) m += '\n'
+    outputStream.writeString(m)
+  }
+}
+
+let logwriter: ReturnType<typeof writer>
+
 export async function startup({ id, version, resourceURI, rootURI = resourceURI.spec }) {
   await waitForZotero()
+
+  const env = Components.classes['@mozilla.org/process/environment;1'].getService(Components.interfaces.nsIEnvironment)
+  let path = env.get('ZOTERO_DEBUG_LOG')
+  log(`log enabled? ${!!path}`)
+
+  if (path === '.') {
+    const dirService = Components.classes['@mozilla.org/file/directory_service;1'].getService(Components.interfaces.nsIProperties)
+    const cwd = dirService.get('CurWorkD', Components.interfaces.nsIFile)
+    path = cwd.path
+  }
+
+  if (path) {
+    const dir = Zotero.File.pathToFile(path)
+    if (!dir.isDirectory()) {
+      log(`${JSON.stringify(path)} is not a directory, log-writer disabled`)
+    }
+    else {
+      log(`logging to ${path}`)
+      const start = new Date
+      const logid = new Date(start.getTime() - start.getTimezoneOffset() * 60000).toISOString().replace('Z', '').replace(/:/g, '.')
+      logwriter = writer(dir, `${logid}.txt`)
+      // Zotero.Debug.addConsoleViewerListener(logwriter)
+      Zotero.Debug.addListener(logwriter)
+    }
+  }
+
   log('registering')
   try {
     DebugLog.register('Debug Log')
@@ -85,6 +132,11 @@ export async function startup({ id, version, resourceURI, rootURI = resourceURI.
 
 export function shutdown() {
   DebugLog.unregister('Debug Log')
+  if (logwriter) {
+    Zotero.Debug.removeListener(logwriter)
+    Zotero.Debug.removeConsoleViewerListener(logwriter)
+    logwriter = null
+  }
 }
 
 export function uninstall() {
